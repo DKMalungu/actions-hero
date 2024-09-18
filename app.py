2340dced5fdae8ccad91b7a1b15cb95257b3b250
root <danielkmalungu@gmail.com>
2024-09-18 14:48:59 +0300

    update: basic application set up

---
+++ app.py
@@ -0,0 +1,9 @@
+from flask import Flask
+app = Flask(__name__)
+@app.route('/')
+def index():
+    return '<h1>Hello World</h1>'
+
+@app.route('/hello/<name>')
+def hello(name):
+    return f'<h1>Hello, {name}!</h1>'
