71ecd0b52837ab16c343e597a4f9089ab605e0dd
root <danielkmalungu@gmail.com>
2024-09-19 11:12:51 +0300

    update: adding additional models

--- backend/models.py
+++ backend/models.py
@@ -6,6 +6,29 @@ class User(db.Model):
    id = db.Column(db.Integer, primary_key=True)
    email = db.Column(
        db.String(64), unique=True, index=True)
+   # Add other fields as needed
+   profile_id = db.Column(db.Integer, db.ForeignKey('profiles.id'))
+   profile = db.relationship('Profile', backref='user')
 
    def __repr__(self):
        return f"<User {self.email} >"
+   
+
+class Profile(db.Model):
+    __tablename__ = 'profiles'
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(64))
+    last_name = db.Column(db.String(64))
+    skills = db.relationship('Skill')
+    
+    def __repr__(self):
+        return f"<Profile {self.first_name} {self.last_name}: #{self.id}>"
+
+class Skill(db.Model):
+    __tablename__ = 'skills'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True), index=True, nullable=False)
+    profile_id = db.Column(db.Integer, db.ForeignKey('profiles.id'))
+    
+    def __repr__(self):
+        return f"<Skill {self.name}: #{self.id}>"
